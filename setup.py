ebf9d7a1f91dc8cac8b7f635ab6f76a3ade710dd
herve <herve.mignot@equancy.com>
2018-12-20 20:15:38 +0100

    Changed classifiers

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ AUTHOR = "HerveMignot"
 EMAIL = "HerveMignot@github.com"
 URL = "http://github.com/HerveMignot/dpaste_magic"
 CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable'
-    'Environment :: Console'
-    'Framework :: IPython'
-    'Framework :: Jupyter'
-    'Intended Audience :: Developers'
-    'Intended Audience :: Education'
-    'Intended Audience :: Science/Research'
-    'License :: OSI Approved :: MIT License'
-    'Operating System :: OS Independent'
-    'Programming Language :: Python'
-    'Programming Language :: Python :: 2'
-    'Programming Language :: Python :: 2.7'
-    'Programming Language :: Python :: 3.5'
-    'Programming Language :: Python :: 3.6'
-    'Programming Language :: Python :: 3.7'
-    'Topic :: Education'
-    'Topic :: Scientific/Engineering'
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Framework :: IPython',
+    'Framework :: Jupyter',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Education',
+    'Topic :: Scientific/Engineering',
 ]
 
 setup(name='dpaste_magic',
